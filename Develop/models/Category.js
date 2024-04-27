e46ad3784f941e497a19b8c0e37648dc1afaebec
madihakhan-hub <144630720+madihakhan-hub@users.noreply.github.com>
2024-04-27 00:17:32 -0500

    added columns

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,12 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -18,3 +25,4 @@ Category.init(
 );
 
 module.exports = Category;
+
--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -18,3 +25,4 @@ Tag.init(
 );
 
 module.exports = Tag;
+
