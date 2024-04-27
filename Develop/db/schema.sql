3988a4f2f4bd5f5f4de92090320808cba3f2b916
madihakhan-hub <144630720+madihakhan-hub@users.noreply.github.com>
2024-04-27 00:19:35 -0500

    added schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,34 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- USE DATABASE
+USE ecommerce_db;
+
+-- CREATE TABLES
+CREATE TABLE category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE product (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE product_tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES product(id),
+  FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
+
